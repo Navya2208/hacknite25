91a42d41dbefb6a229a336c44ca7c2d3f035f3e8
sahiti3636 <p.sahiti.2006@gmail.com>
2025-04-19 23:13:20 +0530

    Create __init__.py

---
+++ src/utils/__init__.py
@@ -0,0 +1,11 @@
+# Import key functions for easier access
+from .helpers import (
+    clean_text,
+    extract_year_from_date,
+    split_genres,
+    extract_duration_info,
+    plot_distribution,
+    generate_wordcloud,
+    get_top_genres,
+    create_user_profile
+)
